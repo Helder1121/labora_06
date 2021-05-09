9a1cca3a2e728ac47ff4ba2916c67351639b4670
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-05-09 13:10:57 -0600

    librerias y clock para puerto f
    subiendo los registro que no guardo anteriormente

--- main.c
+++ main.c
@@ -14,8 +14,6 @@
 #include "driverlib/sysctl.c"
 #include "driverlib/sysctl.h"
 
-uint32_t segundos = 0 ;
-
 int main(void)
 {
     SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ ) ;
@@ -26,31 +24,5 @@ int main(void)
     GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_2) ;//led azul
     GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_3) ;//led verde
 
-    segundos = SysCtlClockGet() / 3 ;
-    while(1){
-        if((GPIOPinRead (GPIO_PORTF_BASE,GPIO_PIN_4 ) & 0x16)==0){
-        while((GPIOPinRead (GPIO_PORTF_BASE,GPIO_PIN_4 ) & 0x16)==0){}
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x0A);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
-        SysCtlDelay (segundos) ;
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x02);
-    }
-    }
+
 }
